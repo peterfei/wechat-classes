0d0c700d5ef05bc504d0e298764dd340d1c3f5eb
peterfei <peterfeispace@gmail.com>
2020-03-30 18:06:10 +0800

    [Feature]编写班级详情页部分页面布局及样式

--- pages/classes-details/classes-details.js
+++ pages/classes-details/classes-details.js
@@ -1,5 +1,25 @@
 const app = getApp();
 
 Page({
-  data: {}
-})
+  data: {
+    src: '../../images/index/default.png',
+    list: [
+      {
+        text: '首页',
+        iconPath: '../../images/icons/home.png',
+        selectedIconPath: '../../images/icons/home-selected.png',
+        dot: true,
+      },
+      {
+        text: '讨论',
+        iconPath: '../../images/icons/talk.png',
+        selectedIconPath: '../../images/icons/talk-selected.png',
+        dot: true,
+      },
+    ],
+  },
+
+  tabChange(e) {
+    console.log('tab change', e);
+  },
+});
