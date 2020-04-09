76b3e4a8aca65cb1b45f53360d951a0f29489535
peterfei <peterfeispace@gmail.com>
2020-04-09 15:27:36 +0800

    [Refactor]针对小程序富文本编辑器中的下载优化

--- compoments/wxParse/wxParse.js
+++ compoments/wxParse/wxParse.js
@@ -1,5 +1,5 @@
 import HtmlToJson from '../../utils/html2json';
-
+import {wwwUrl} from '../../global';
 // 获取屏幕的宽高
 let realWindowWidth = 0;
 let realWindowHeight = 0;
@@ -7,11 +7,11 @@ let realWindowHeight = 0;
 (function() {
   wx.getSystemInfo({
     success(res) {
-      realWindowWidth = res.windowWidth
-      realWindowHeight = res.windowHeight
-    }
-  })
-})()
+      realWindowWidth = res.windowWidth;
+      realWindowHeight = res.windowHeight;
+    },
+  });
+})();
 
 Component({
   properties: {
@@ -19,16 +19,19 @@ Component({
       type: null,
       observer(val) {
         if (val) {
-          if (typeof val === 'string') { // 初始为html富文本字符串
-            this._parseHtml(val)
-          } else if (Array.isArray(val)) { // html 富文本解析成节点数组
-            this.setData({ nodesData: val })
-          } else { // 其余为单个节点对象
-            const nodesData = [ val ]
-            this.setData({ nodesData })
+          if (typeof val === 'string') {
+            // 初始为html富文本字符串
+            this._parseHtml(val);
+          } else if (Array.isArray(val)) {
+            // html 富文本解析成节点数组
+            this.setData({nodesData: val});
+          } else {
+            // 其余为单个节点对象
+            const nodesData = [val];
+            this.setData({nodesData});
           }
         }
-      }
+      },
     },
   },
 
@@ -39,21 +42,21 @@ Component({
 
   methods: {
     _parseHtml(html, bindName) {
-      bindName = 'wxParseData'
+      bindName = 'wxParseData';
 
       //存放html节点转化后的json数据
-      const transData = HtmlToJson.html2json(html, bindName)
+      const transData = HtmlToJson.html2json(html, bindName);
 
-      transData.view = {}
-      transData.view.imagePadding = 0
+      transData.view = {};
+      transData.view.imagePadding = 0;
 
       this.setData({
         nodesData: transData.nodes,
         bindData: {
-          [bindName]: transData
-        }
-      })
-      console.log(this.data)
+          [bindName]: transData,
+        },
+      });
+      console.log(this.data);
     },
 
     /**
@@ -62,13 +65,13 @@ Component({
      */
     wxParseImgLoad(e) {
       // 获取当前的image node节点
-      const { from: tagFrom } = e.target.dataset || {}
+      const {from: tagFrom} = e.target.dataset || {};
       if (typeof tagFrom !== 'undefined' && tagFrom.length > 0) {
-        const { width, height } = e.detail
+        const {width, height} = e.detail;
 
         //因为无法获取view宽度 需要自定义padding进行计算，稍后处理
-        const recal = this._wxAutoImageCal(width, height)
-        this.setData({ width: recal.imageWidth })
+        const recal = this._wxAutoImageCal(width, height);
+        this.setData({width: recal.imageWidth});
       }
     },
 
@@ -78,23 +81,27 @@ Component({
      * @param {*} originalHeight
      */
     _wxAutoImageCal(originalWidth, originalHeight) {
-      let windowWidth = 0, windowHeight = 0;
-      let autoWidth = 0, autoHeight = 0;
-      const results = {}
-      windowWidth = realWindowWidth
-      windowHeight = realWindowHeight
+      let windowWidth = 0,
+        windowHeight = 0;
+      let autoWidth = 0,
+        autoHeight = 0;
+      const results = {};
+      windowWidth = realWindowWidth;
+      windowHeight = realWindowHeight;
 
       // 判断按照哪种方式进行缩放
-      if (originalWidth > windowWidth) { //在图片width大于手机屏幕width时候
-        autoWidth = windowWidth
-        autoHeight = (autoWidth * originalHeight) / originalWidth
-        results.imageWidth = autoWidth
-        results.imageHeight = autoHeight
-      } else { // 否则展示原来数据
-        results.imageWidth = originalWidth
-        results.imageHeight = originalHeight
+      if (originalWidth > windowWidth) {
+        //在图片width大于手机屏幕width时候
+        autoWidth = windowWidth;
+        autoHeight = (autoWidth * originalHeight) / originalWidth;
+        results.imageWidth = autoWidth;
+        results.imageHeight = autoHeight;
+      } else {
+        // 否则展示原来数据
+        results.imageWidth = originalWidth;
+        results.imageHeight = originalHeight;
       }
-      return results
+      return results;
     },
 
     /**
@@ -102,14 +109,38 @@ Component({
      * @param {*} e
      */
     wxParseTagATap(e) {
-      const { src } = e.currentTarget.dataset
-
+      const {src} = e.currentTarget.dataset;
+      console.log(`%c%s`, 'color:blue', src);
+      wx.downloadFile({
+        url: wwwUrl + src,
+        success(res) {
+          console.log(`...正在下载`, JSON.stringify(res));
+          wx.saveFile({
+            tempFilePath: res.tempFilePath,
+            success: function(res) {
+              var saveFilePath = res.savedFilePath;
+              wx.showToast({
+                title: '保存成功！',
+                duration: 2000,
+              });
+              console.log('下载成功,保存至:', saveFilePath);
+            }, //可以将saveFilePath写入到页面数据中
+            fail: function(res) {
+              console.log(res);
+				wx.showToast({title:'因小程序下载受限,请转至app下载',icon:"none"})
+            },
+          });
+        },
+        fail(err) {
+          console.log(`...下载失败`, JSON.stringify(err));
+        },
+      });
       // 采用递归组件方式渲染，不能通过triggerEvent方式向父级传参，可以获取当前页面调用页面方法处理
-      const curPages =  getCurrentPages();
-      const currentPage = curPages[curPages.length - 1]
+      const curPages = getCurrentPages();
+      const currentPage = curPages[curPages.length - 1];
       if (currentPage) {
-        currentPage.handleTagATap && currentPage.handleTagATap(src)
+        currentPage.handleTagATap && currentPage.handleTagATap(src);
       }
-    }
-  }
-})
+    },
+  },
+});
