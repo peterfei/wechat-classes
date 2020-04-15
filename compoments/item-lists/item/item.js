da11e350c21240f4faeaa82021bb33772b30f970
peterfei <peterfeispace@gmail.com>
2020-04-15 09:33:16 +0800

    [Refactor]重构,抽取公共lists item 做为组件

---
+++ compoments/item-lists/item/item.js
@@ -0,0 +1,31 @@
+// compoments/item-lists/item/item.js
+import {baseUrl, baseImageUrl} from '../../../global';
+Component({
+  options: {
+    multipleSlots: true, // 在组件定义时的选项中启用多slot支持
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    itemObj: {type: Object},
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+  lifetimes: {
+    attached: function() {
+      this.setData({
+        baseUrl: baseUrl,
+        baseImageUrl: baseImageUrl,
+      });
+    },
+  },
+});
