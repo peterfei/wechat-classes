c6b84679f27407d96d898781d3270c2aba269ed0
peterfei <peterfeispace@gmail.com>
2020-05-09 14:37:42 +0800

    [Style]考题样式修改

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 const baseUrl = "https://v2.api.ibodao.com/"
 const wwwUrl = "http://www.ibodao.com/"
-const token = "378a1da4c8113120ac6cd0deffd340bb41ececc97e5145e1f8f8d315cacd9c56" //模拟token
+const token = "f228b7bf4c69a4ef0d9af27309e78b5162cb738a34ca18f3f13e1b4bab7bb4a9" //模拟token
 const contentType = {"Content-Type":"application/x-www-form-urlencoded"}
 const baseImageUrl = "http://static.ibodao.com/"
 export {baseUrl,token,contentType,baseImageUrl,wwwUrl}
